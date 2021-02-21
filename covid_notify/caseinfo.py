c75f69e271d8c371a7ea7022b2a0d78031fef751
m4cola <mike@mdinicola.com>
2021-02-20 22:41:19 -0500

    fix date comparison

--- covid_notify/caseinfo.py
+++ covid_notify/caseinfo.py
@@ -1,8 +1,10 @@
 import pandas
 from datetime import datetime, timedelta
+from pytz import timezone
 
 class CaseInfo:
     _ONTARIO_COVID_RESULTS_URL = "https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv"
+    _eastern_timezone = timezone('Canada/Eastern')
 
     def __init__(self, region):
         self.region = region
@@ -19,7 +21,7 @@ class CaseInfo:
 
     def _get_reported_date(self, data):
         reported_date = data['Reported Date'].iat[-1]
-        return datetime.strptime(reported_date, '%Y-%m-%d')
+        return self._eastern_timezone.localize(datetime.strptime(reported_date, '%Y-%m-%d'))
 
     def _get_new_cases(self, data):
         today_cases = data['Total Cases'].iat[-1]
@@ -28,7 +30,8 @@ class CaseInfo:
         return "{:.0f}".format(new_cases)
 
     def is_stale(self):
-        return not (datetime.today().date() == self.reported_date.date() and datetime.today() >= self.reported_date)
+        today = datetime.now(self._eastern_timezone)
+        return not (today >= self.reported_date)
 
     def set_messsage(self, message):
         self.message = message
